aa50a2de23294cbb4dd0018bd16b7854a397df56
Nikita Vasilchenko <rexona8@yandex.ru>
2023-02-24 22:21:09 +0300

    added task19 decision

--- DomZad3/task19/Program.cs
+++ DomZad3/task19/Program.cs
@@ -5,3 +5,19 @@
 // 12821 -> да
 // 23432 -> да
 
+System.Console.Write("Введите число: ");
+string? number = Console.ReadLine();
+int lenght = number!.Length;
+if (lenght == 5)
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        System.Console.WriteLine("Является палиндромом");
+    }
+    else
+    {
+        System.Console.WriteLine("Не является палиндромом");
+    }
+else
+{
+    System.Console.WriteLine($"{number} - число не пятизначное");
+}
