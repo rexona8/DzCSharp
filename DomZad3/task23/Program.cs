50a94ee75a4c2023d7e9a05a389f95b5c3d18345
Nikita Vasilchenko <rexona8@yandex.ru>
2023-02-24 21:13:06 +0300

    added task23 decision

--- DomZad3/task23/Program.cs
+++ DomZad3/task23/Program.cs
@@ -4,3 +4,10 @@
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
 
+System.Console.WriteLine("Введите число: ");
+int number = Math.Abs(Convert.ToInt32(System.Console.ReadLine()));
+
+for (int i = 1; i <= number; i++)
+{
+    System.Console.WriteLine(Math.Pow(i, 3) + "");
+}
